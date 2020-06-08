ce47f85caa648e58080fc3ba14f23ecf9caf5f55
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-06-08 22:36:02 +0500

    First react class work

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Hello.css'
+
+function Hello() {
+  return <div className = "myname">
+          Hello from Hello component
+          
+          </div>
+    
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+  <App name = "Waqas Mahmood" age = {35*1}/>
+  
+  </div>,
   document.getElementById('root')
 );
 
