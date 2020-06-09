82901135a4057029b8e8dfcb4325f10b70089df0
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-06-09 19:33:40 +0500

    changes for assignment

--- src/App.js
+++ src/App.js
@@ -5,9 +5,28 @@ import Hello from './Hello.js';
 
 function App(props) {
   return <div>
-          Hello from App.js by {props.name} and age is {props.age}
+          Hello from  <strong>{props.name}</strong> and age is {props.age}
           <br/>
           <Hello></Hello>
+          <h1>
+            About Me
+          </h1>
+          <h3>
+            About my Career
+          </h3>
+          <ol>
+            <li>I am a bachelor in Commerce from Karachi university</li>
+            <li>I have done my Chartered Accountancy in 2008 and done my articleship from KPMG</li>
+            <li>I am currently working as a senior finance manager in Pakistan Cables Ltd.</li>
+          </ol>
+          <h3>
+            About my Personality
+          </h3>
+          <ul>
+            <li>I have a great sense of responsibilty</li>
+            <li>I am good in managing things</li>
+            <li>I am here to learn new things</li>
+          </ul>
 
           
           </div>
